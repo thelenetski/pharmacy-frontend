663fab9eddde52779c2cd784c57011cd15fb07e3
thelenetski <49438367+thelenetski@users.noreply.github.com>
2025-04-19 21:44:03 +0300

    fix type

--- src/components/SharedLayout/SharedLayout.tsx
+++ src/components/SharedLayout/SharedLayout.tsx
@@ -1,8 +1,8 @@
 import { ReactNode, Suspense, useEffect, useState } from 'react';
 import style from './SharedLayout.module.scss';
 import Loader from '../Loader/Loader';
-import { useSelector } from 'react-redux';
-import { selectTypeModal } from '../../redux/modal/selectors';
+// import { useSelector } from 'react-redux';
+// import { selectTypeModal } from '../../redux/modal/selectors';
 import Header from '../Header/Header';
 import { useLocation } from 'react-router-dom';
 
@@ -11,7 +11,7 @@ interface SharedLayoutProps {
 }
 
 const SharedLayout: React.FC<SharedLayoutProps> = ({ children }) => {
-  const type = useSelector(selectTypeModal);
+  // const type = useSelector(selectTypeModal);
   const location = useLocation();
   const [headerType, setHeaderType] = useState('simple');
 
