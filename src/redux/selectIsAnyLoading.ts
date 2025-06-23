ab23583e1755d77002cffd6730b67e63e278c118
thelenetski <49438367+thelenetski@users.noreply.github.com>
2025-06-23 22:01:45 +0300

    refactor global loading

--- src/redux/selectIsAnyLoading.ts
+++ src/redux/selectIsAnyLoading.ts
@@ -1,11 +1,14 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from './store';
 
-export const selectIsAnyLoading = createSelector(
+const loadingSelectors = [
   (state: RootState) => state.order.loading.order,
   (state: RootState) => state.product.loading.product,
   (state: RootState) => state.supplier.loading.supplier,
   (state: RootState) => state.customers.loading.customer,
-  (orderLoading, productLoading, supplierLoading, customersLoading) =>
-    orderLoading || productLoading || supplierLoading || customersLoading
+];
+
+export const selectIsAnyLoading = createSelector(
+  loadingSelectors,
+  (...loadings) => loadings.some(Boolean)
 );
