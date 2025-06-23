eec32d9ccbf6dd782b34c9c4a9c3686638051772
thelenetski <49438367+thelenetski@users.noreply.github.com>
2025-06-23 20:30:02 +0300

    fix order redux

--- src/redux/orders/selectors.ts
+++ src/redux/orders/selectors.ts
@@ -4,5 +4,4 @@ export const selectOrderData = (state: RootState) => state.order.data;
 export const selectOrderPage = (state: RootState) => state.order.page;
 export const selectOrderTotalPages = (state: RootState) =>
   state.order.totalPages;
-export const selectOrderFilters = (state: RootState) => state.order.filters;
 export const selectOrderLoading = (state: RootState) => state.order.loading;
